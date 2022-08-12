0a2daad8b384ed12077d0b86de9995a5fc18f7fb
Aholu Samuel <samuelaholu15@gmail.com>
2022-08-12 20:07:31 +0100

    Update wait.php

--- wait.php
+++ wait.php
@@ -1,5 +1,5 @@
 <?php
-$con = mysqli_connect("localhost", "healthgo_website", "mypasswordis100%safe", "hellthgo_waitlist");
+$con = mysqli_connect("localhost", "hellthgo_website", "mypasswordis100%safe", "hellthgo_waitlist");
 function row_count($result)
 {
 
@@ -14,9 +14,8 @@ if (isset($_POST['u_name']) && isset($_POST['email'])) {
     $u_name = $_POST['u_name'];
     $email = $_POST['email'];
 
-    
-    $sql = "INSERT INTO healthgo_waitlist (name, email) VALUES ('$u_name', '$email')";
-    $result = mysqli_query($con, $sql);
+    $sqlt = "INSERT INTO healthgo_waitlist(`name`, `email`)";
+    $sqlt .= " VALUES('$u_name', '$email')";
+    $result = mysqli_query($con, $sqlt);
     echo '<p style="text-align:center">Thank you for joining our WaitList</p>';
 }
-?>
